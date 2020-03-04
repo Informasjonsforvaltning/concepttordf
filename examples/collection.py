305443b2517274236c18418aa3623d54d1bb4c76
Stig B. Dørmænen <stigbd@gmail.com>
2020-03-04 21:22:01 +0100

    Add collection example

---
+++ examples/collection.py
@@ -0,0 +1,32 @@
+from concepttordf.collection import Collection
+from concepttordf.concept import Concept
+
+collection = Collection()
+collection.identifier = "http://example.com/collections/1"
+collection.name = {}
+collection.name['en'] = "A concept collection"
+collection.name['nb'] = "En begrepssamling"
+collection.description = {}
+collection.description['en'] = (
+    "This collection collects a sample of our finest concepts"
+    )
+collection.description['nb'] = (
+    "Denne samlingen inneholder et utvalg av våre beste begreper"
+    )
+collection.publisher = "https://example.com/publishers/1"
+
+# Add concepts to collection
+collection.members = []
+
+c1 = Concept()
+c1.identifier = "http://example.com/concepts/1"
+
+c2 = Concept()
+c2.identifier = "http://example.com/concepts/2"
+
+collection.members.append(c1)
+collection.members.append(c2)
+
+print(collection.to_rdf().decode())
+
+# prints:
