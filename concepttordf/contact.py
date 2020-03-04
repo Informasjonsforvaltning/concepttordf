c3fe73326f1832a1210c96a0a4bea37c8bebe676
Stig B. Dørmænen <stigbd@gmail.com>
2020-03-04 17:33:47 +0100

    Add support for contact to_rdf

---
+++ concepttordf/contact.py
@@ -0,0 +1,75 @@
+from rdflib import Graph, Literal, BNode, Namespace, RDF, URIRef
+
+DCT = Namespace('http://purl.org/dc/terms/')
+SKOSXL = Namespace('http://www.w3.org/2008/05/skos-xl#')
+VCARD = Namespace('http://www.w3.org/2006/vcard/ns#')
+SKOS = Namespace('http://www.w3.org/2004/02/skos/core#')
+SKOSNO = Namespace('http://difi.no/skosno#')
+
+
+class Contact:
+    """A class representing a contact """
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @name.setter
+    def name(self, name: str):
+        self._name = name
+
+    @property
+    def email(self) -> str:
+        return self._email
+
+    @email.setter
+    def email(self, email: str):
+        self._email = email
+
+    @property
+    def url(self) -> str:
+        return self._url
+
+    @url.setter
+    def url(self, url: str):
+        self._url = url
+
+    def to_rdf(self, format='turtle') -> str:
+        """Maps the contact to rdf and returns a serialization
+           as a string according to format"""
+
+        _g = _add_contact_to_graph(self)
+
+        return _g.serialize(format=format, encoding='utf-8')
+
+
+def _add_contact_to_graph(contact: Contact) -> Graph:
+    """Adds the concept to the Graph g and returns g"""
+
+    g = Graph()
+
+    g.bind('vcard', VCARD)
+
+    if hasattr(contact, 'identifier'):
+        _contact = URIRef(contact.identifier)
+    else:
+        _contact = BNode()
+
+    g.add((_contact, RDF.type, VCARD.Organization))
+
+    # name
+    if hasattr(contact, 'name'):
+        for key in contact.name:
+            g.add((_contact, VCARD.hasOrganizationName,
+                   Literal(contact.name[key], lang=key)))
+
+    # email
+    if hasattr(contact, 'email'):
+        g.add((_contact, VCARD.hasEmail,
+               URIRef('mailto:' + contact.email)))
+    # url
+    if contact.url:
+        g.add((_contact, VCARD.hasURL,
+               URIRef(contact.url)))
+
+    return g
---
+++ tests/test_contact.py
@@ -0,0 +1,66 @@
+from concepttordf.contact import Contact
+import json
+from rdflib import Graph
+from rdflib.compare import isomorphic, graph_diff
+
+
+def test_contact_to_rdf_should_return_skos_contact():
+    with open('./tests/contact.json') as json_file:
+        data = json.load(json_file)
+        _contact = data['contact']
+    contact = Contact()
+    contact.identifier = _contact['identifier']
+    contact.name = _contact['name']
+    contact.email = _contact['email']
+    contact.url = _contact['url']
+
+    g1 = Graph()
+    g1.parse(data=contact.to_rdf(), format='turtle')
+    g2 = Graph().parse("tests/contact.ttl", format='turtle', encoding='utf-8')
+
+    _isomorphic = isomorphic(g1, g2)
+    if not _isomorphic:
+        _dump_diff(g1, g2)
+        pass
+    assert _isomorphic
+
+
+def test_contact_without_id_to_rdf_should_return_skos_contact():
+    with open('./tests/contact.json') as json_file:
+        data = json.load(json_file)
+        _contact = data['contact']
+    contact = Contact()
+    contact.name = _contact['name']
+    contact.email = _contact['email']
+    contact.url = _contact['url']
+
+    g1 = Graph()
+    g1.parse(data=contact.to_rdf(), format='turtle')
+    g2 = Graph().parse("tests/contact.ttl", format='turtle', encoding='utf-8')
+
+    assert len(g1) == len(g2)
+
+
+# ---------------------------------------------------------------------- #
+# Util for displaying debug information
+
+def _dump_diff(g1, g2):
+    in_both, in_first, in_second = graph_diff(g1, g2)
+    print("\nin both:")
+    _dump_turtle_sorted(in_both)
+    print("\nin first:")
+    _dump_turtle_sorted(in_first)
+    print("\nin second:")
+    _dump_turtle_sorted(in_second)
+
+
+def _dump_turtle_sorted(g):
+    for l in sorted(g.serialize(format='turtle').splitlines()):
+        if l:
+            print(l.decode())
+
+
+def _dump_turtle(g):
+    for l in g.serialize(format='turtle').splitlines():
+        if l:
+            print(l.decode())
