d889c60f1551a423d3979b37754c436396395693
Stig B. Dørmænen <stigbd@gmail.com>
2020-03-04 22:30:36 +0100

    Fix description in setup.py

--- setup.py
+++ setup.py
@@ -4,18 +4,21 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="concepttordf-stigbd",  # Replace with your own username
-    version="0.0.3",
+    name="concepttordf-stigbd",
+    version="0.0.5",
     author="Stig B. Dørmænen",
     author_email="stigbd@gmail.com",
-    description="A simple library to parse openAPI specs",
+    description=(
+                 "A small Python library for mapping a concept collection"
+                 "to the skos-ap-no specification"
+                 ),
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Informasjonsforvaltning/concepttordf",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.8',
