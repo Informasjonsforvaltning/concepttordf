166dad1182afb194949bf017418a1e8c581293a8
Stig B. Dørmænen <stigbd@gmail.com>
2020-03-22 12:06:41 +0100

    Release new version

--- src/concepttordf/__init__.py
+++ src/concepttordf/__init__.py
@@ -40,6 +40,15 @@ Example:
     True
 
 """
+try:
+    from importlib.metadata import version, PackageNotFoundError  # type: ignore
+except ImportError:  # pragma: no cover
+    from importlib_metadata import version, PackageNotFoundError  # type: ignore
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
+
 from .alternativformulering import AlternativFormulering
 from .associativerelation import AssociativeRelation
 from .betydningsbeskrivelse import Betydningsbeskrivelse
