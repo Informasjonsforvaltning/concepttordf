e8e91d76e6b96d775c24d97ee61e73cce6ddec53
Nils Ove Tendenes <nils.ove@tendenes.com>
2026-07-02 12:04:30 +0200

    chore: switch from safety to pip-audit

--- noxfile.py
+++ noxfile.py
@@ -49,12 +49,10 @@ def lint(session: Session) -> None:
 
 
 @session(python="3.12")
-def safety(session: Session) -> None:
-    """Scan dependencies for insecure packages."""
-    session.install("poetry-plugin-export")
-    session.install("safety")
-    requirements = session.poetry.export_requirements()
-    session.run("safety", "check", "--full-report", f"--file={requirements}")
+def audit(session: Session) -> None:
+    """Run pip-audit security audit."""
+    session.install("pip-audit")
+    session.run("pip-audit")
 
 
 @session(python=["3.12"])
